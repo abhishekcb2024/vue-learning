ec8098efc6f4e03bd0aa61d450936cbbf0f72095
AjayCerebulb <166582742+ajaypawar2211@users.noreply.github.com>
2025-02-07 14:47:37 +0530

    video 19 done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <script setup>
 import TheWelcome from '@/components/TheWelcome.vue'
-import { onMounted, ref } from 'vue'
+import { useFlash } from '@/composables/useFlash'
+import { ref } from 'vue'
 
 let message = ref('hello')
 
 let doSomething = () => {
   alert('abhi')
 }
+
+let { flash } = useFlash()
 </script>
 
 <template>
@@ -15,5 +18,7 @@ let doSomething = () => {
     <h1>{{ message }}</h1>
     <input type="text" v-model="message" />
     <button @click="doSomething">Submit</button>
+    <br />
+    <button @click="flash('it is worked')">Click here</button>
   </main>
 </template>
